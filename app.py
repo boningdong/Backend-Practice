e9aa9f4567c5d756390857c704d7d04e21651ee8
Boning Dong <dbndbn9538@hotmail.com>
2018-04-09 02:37:26 -0700

    Added post save string functions

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def save_string(str):
     db.session.commit()
 
     strBuilder = ""
-    entries = db.session.query.order_by(Info.info).all()
+    entries = Info.query.order_by(Info.info).all()
     for entry in entries:
         strBuilder += str(entry)
 
